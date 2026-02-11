046178196b62a35c0965c4acd2fcc111b7e84360
oc-Liam-Cole-25 <liam.cole@student.onslow.school.nz>
2026-02-11 21:27:55 +0000

    Cool thingy1

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,27 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//Liam cole 8/02/2026
+//Practice and revision 
+var Length = 6.0
+var Width = 4.5
+var Height = 2.7
 
-@main
-struct SwiftPlayground {
-    static func main() {
-        print("Hello, world!")
-    }
-}
+print("Enter room length:")
+if let input = readLine()  {
+    // store length, do further calculations
+    Length = Double(input)!
+} 
+
+print("Enter room width:")
+if let input = readLine()  {
+    // store length, do further calculations
+    Width = Double(input)!
+} 
+
+print("room area: \(Length*Width)")
+
+print("Enter room height:")
+if let input = readLine()  {
+    // store length, do further calculations
+    Height = Double(input)!
+} 
+
+print("room volume: \(Length*Width*Height)")
