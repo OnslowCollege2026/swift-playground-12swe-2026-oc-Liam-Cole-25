a695eb9adc85156045a5dac7bee179913549070b
oc-Liam-Cole-25 <liam.cole@student.onslow.school.nz>
2026-02-13 01:56:57 +0000

    Feb 13, 2026, 1:56 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,9 @@
 var Standingreach = 225
+//this is how high you can reach
 var Rimheight = 305
+//how high a nba rim is
 let Gapfordunk = 15
+//how much space is required for a dunk
 //these are the averages
 print("Enter your standing reach in cm:")
 if let Input = readLine(){
