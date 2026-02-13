5eef29b01d65148a9df4d534ae506f7b5a336003
oc-Liam-Cole-25 <liam.cole@student.onslow.school.nz>
2026-02-13 01:24:46 +0000

    Feb 13, 2026, 1:24 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,27 +1,8 @@
-//Liam cole 8/02/2026
-//Practice and revision 
-var Length = 6.0
-var Width = 4.5
-var Height = 2.7
-
-print("Enter room length:")
-if let input = readLine()  {
-    // store length, do further calculations
-    Length = Double(input)!
-} 
-
-print("Enter room width:")
-if let input = readLine()  {
-    // store length, do further calculations
-    Width = Double(input)!
-} 
-
-print("room area: \(Length*Width)")
-
-print("Enter room height:")
-if let input = readLine()  {
-    // store length, do further calculations
-    Height = Double(input)!
-} 
-
-print("room volume: \(Length*Width*Height)")
+var Standingreach = 225
+var Rimheight = 305
+let Gapfordunk = 15
+//these are the averages
+print("Enter your standing reach in cm:")
+if let Input = readLine(){
+Standingreach = Double(Input)
+}
