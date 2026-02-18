517c57e58b21616f829490915a25d4ed8cf0a347
oc-Liam-Cole-25 <liam.cole@student.onslow.school.nz>
2026-02-18 21:27:16 +0000

    DUNK CALC

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,11 +1,20 @@
-var Standingreach = 225
+var Standingreach:Double = 225
 //this is how high you can reach
-var Rimheight = 305
+var Rimheight:Double = 305
 //how high a nba rim is
-let Gapfordunk = 15
+let Gapfordunk:Double = 20
 //how much space is required for a dunk
 //these are the averages
 print("Enter your standing reach in cm:")
-if let Input = readLine(){
-Standingreach = Double(Input)
+if let reachinput:Double = Double(readLine()!) {
+    Standingreach=reachinput
 }
+print("Your standing reach is \(Standingreach) cm")
+
+print("Enter the rim height in cm:")
+if let riminput:Double = Double(readLine()!) {
+    Rimheight=riminput
+}
+print("the rim is \(Rimheight) cm high")
+print("Now we will find out the minimum vertical for you to dunk")
+print("you need a \(Rimheight+Gapfordunk-Standingreach) cm vertical to dunk")
