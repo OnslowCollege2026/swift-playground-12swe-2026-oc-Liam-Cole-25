0f92aa4d94cbfc8aef9dd3e606de230761012173
oc-Liam-Cole-25 <liam.cole@student.onslow.school.nz>
2026-04-22 23:11:59 +0000

    Spell 1

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,12 @@
-func roomVolume() -> Double {
-    return 4.7 * 6 * 2.1
+var kumaraWeightKg = 50.0
+var countOfBags = 5000.0
+var kumaraWeightKgSold = 0.1
+var countOfBagsSold = 1
+func kumarasInABagCount (_ prompt: String, cancelPhrase: String){
+    while true {
+        print(prompt, terminator: "")
+        guard let kumarasKg:Double = readLine(), kumarasKg.count > 0.1 else {
+            
+        }
+    }
 }
-
-let volume = roomVolume()
-print("Room volume: \(volume) m³")
