ed2222225aa17c2060c364679cabbe325dbe8e79
oc-Liam-Cole-25 <liam.cole@student.onslow.school.nz>
2026-04-28 00:21:21 +0000

    spell 2

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,11 +2,11 @@ var kumaraWeightKg = 50.0
 var countOfBags = 5000.0
 var kumaraWeightKgSold = 0.1
 var countOfBagsSold = 1
-func kumarasInABagCount (_ prompt: String, cancelPhrase: String){
+func kumarasInABagCount (_ prompt: String, cancelPhrase: String) -> String?{
     while true {
         print(prompt, terminator: "")
-        guard let kumarasKg:Double = readLine(), kumarasKg.count > 0.1 else {
-            
+        guard let kumarasKg: Double = Double(readLine()!), kumarasKg > 0.099 else {
+            continue
         }
     }
 }
